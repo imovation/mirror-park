16f61c6333dd4aa044e951e17d9d027c24183fc9
imovation <283284956@qq.com>
2026-06-17 12:26:45 +0800

    feat: make SceneCanvas fog/background/bloom follow timeMode

--- src/components/scene/SceneCanvas.tsx
+++ src/components/scene/SceneCanvas.tsx
@@ -5,28 +5,42 @@ import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import ParticleBg from './ParticleBg'
 import CameraController from './CameraController'
 import SceneInfo from './SceneInfo'
+import { useTimeModeStore } from '@/stores/useStyleStore'
+import { DAY_NIGHT } from '@/config/dayNightTheme'
 import { SCENE } from '@/utils/constants'
 
 interface SceneCanvasProps {
   children: ReactNode
 }
 
+function SceneContent() {
+  const timeMode = useTimeModeStore((s) => s.timeMode)
+  const cfg = DAY_NIGHT[timeMode]
+  return (
+    <>
+      <fog attach="fog" args={[cfg.fog.color, SCENE.FOG_NEAR, SCENE.FOG_FAR]} />
+      <ParticleBg />
+      <CameraController />
+      <EffectComposer>
+        <Bloom luminanceThreshold={0.5} luminanceSmoothing={0.9} mipmapBlur intensity={cfg.bloom.intensity} />
+      </EffectComposer>
+    </>
+  )
+}
+
 export default function SceneCanvas({ children }: SceneCanvasProps) {
+  const timeMode = useTimeModeStore((s) => s.timeMode)
+  const cfg = DAY_NIGHT[timeMode]
   return (
     <div className="scene-loading" style={{ width: '100%', height: '100%', position: 'relative' }}>
       <Canvas
         camera={{ fov: 50, near: 0.1, far: 1000 }}
-        style={{ background: 'linear-gradient(180deg, #0a1628 0%, #16213e 100%)' }}
+        style={{ background: cfg.canvas.background }}
       >
-        <fog attach="fog" args={[SCENE.FOG_COLOR, SCENE.FOG_NEAR, SCENE.FOG_FAR]} />
+        {children}
         <Suspense fallback={null}>
-          {children}
-          <ParticleBg />
-          <CameraController />
+          <SceneContent />
         </Suspense>
-        <EffectComposer>
-          <Bloom luminanceThreshold={0.5} luminanceSmoothing={0.9} mipmapBlur intensity={0.6} />
-        </EffectComposer>
       </Canvas>
       <SceneInfo />
     </div>
